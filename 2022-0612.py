dc83006d77a04aa2f8cf28ff4702448bb948c695
miyaryo1212 <83214456+miyaryo1212@users.noreply.github.com>
2022-06-13 00:19:33 +0900

    2022-0612 *2

--- 2022-0612.py
+++ 2022-0612.py
@@ -1,10 +1,15 @@
 # 素数計算
 print("素数計算")
-input_number = int(input("どこまで計算する？数字を入力 (N ≧ 4) >>> "))
+input_number = int(input("どこまで計算する？数字を入力 (N ≧ 5) >>> "))
+totarl_calc = 0
 for i in range(4, input_number + 1):
     for j in range(2, i + 1):
+        totarl_calc += 1
         if j == i:
             print(i)
             break
         if i % j == 0:
             break
+print("==============================")
+print(f'{"合計試行回数："}{str(totarl_calc)}')
+print("==============================")
