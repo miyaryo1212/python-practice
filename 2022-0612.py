b0f0a83c0a6479a69a49ab77c688bffe7f9e0d5a
miyaryo1212 <83214456+miyaryo1212@users.noreply.github.com>
2022-06-13 01:19:35 +0900

    2022-0612 *3

--- 2022-0612.py
+++ 2022-0612.py
@@ -1,15 +1,27 @@
 # 素数計算
+import time
+
 print("素数計算")
+
 input_number = int(input("どこまで計算する？数字を入力 (N ≧ 5) >>> "))
-totarl_calc = 0
+
+t0 = time.time()
+
+total_calc = 0
 for i in range(4, input_number + 1):
     for j in range(2, i + 1):
-        totarl_calc += 1
+        total_calc += 1
         if j == i:
             print(i)
             break
         if i % j == 0:
             break
+
+t1 = time.time()
+total_time = t1 - t0
+
 print("==============================")
-print(f'{"合計試行回数："}{str(totarl_calc)}')
+print(f'{"合計計算回数："}{str(total_calc)}')
+print(f'{"経過時間 (計算)："}{str(total_time)}')
+print(f'{"calc / time = "}{total_calc / total_time}')
 print("==============================")
