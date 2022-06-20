76113e07a4aaba06059192fe728468f89853550f
miyaryo1212 <83214456+miyaryo1212@users.noreply.github.com>
2022-06-21 01:09:07 +0900

    2022-0620

---
+++ 2022-0620.py
@@ -0,0 +1,9 @@
+# 改行を含んだ文字列
+# Twitterのヘッダー用のネタ (ターミナルの画像)
+print("""
+UserName: \tmiyaryo1212
+UserID: \t@miyaryo1212
+
+Windows Insider (Dev Channel) / Python 3 (OpenCV) / WebVR, AR (A-Frame, AR.js) / 
+Lenovo Yoga 660 User / Minecraft 1.18 (Java)
+""")
